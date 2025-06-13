525512932ce52d57a36e81a1b6f5f7b23535c77b
1dylanleray <dleray77@outlook.fr>
2025-06-13 14:03:21 +0200

    first commit

---
+++ internal/checker/check.go
@@ -0,0 +1,34 @@
+package checker
+
+import (
+	"net/http"
+	"time"
+)
+
+// CheckResult en majuscule pour exporter le type
+type CheckResult struct {
+	Target string
+	Status string
+	Err    error
+}
+
+func CheckURL(url string) CheckResult {
+	client := http.Client{
+		Timeout: time.Second * 3,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return CheckResult{
+			Target: url,
+			Err:    &UnreachableURLError{URL: url, Err: err},
+		}
+
+	}
+	defer resp.Body.Close()
+
+	return CheckResult{
+		Target: url,
+		Status: resp.Status,
+	}
+}
